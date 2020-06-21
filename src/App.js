b52f04234b9ed3cdba321e60d9d1abcd93429555
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2020-06-21 14:57:35 -0500

    First example of using the useState react hook and how it differs from class based components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
-      { name: "Joe", age:"28"},
-      { name: "Matt", age:"31"}
-    ],
-    otherState: "Some other value"
-  }
+      { name: "Joe", age:28},
+      { name: "Matt", age:31}
+    ]
+  }); 
 
-  switchNameHandler = () => {
-    // console.log("Was Clicked!!!!!!!");
-    this.setState({ 
-      persons: [
-      { name: "Joseph", age:"28"},
-      { name: "Matt", age:"39"}
-      ]
-    } )
-  }
+  const [otherState, setOtherState] = useState({
+    otherState:"Some other value"
+  })
+
+  console.log(personsState, otherState)
   
-  render() {
-    return (
-      <div className="App">
-        <hi>Hi, I'm a react app</hi>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Running </Person>
-        <Person/>
-      </div>
-      
-    );
-    //return React.createElement('div', null, 'h1', 'HI, I\'m a react app!!!')
-  }
-}
+  const switchNameHandler = () => {
+    // console.log("Was Clicked!!!!!!!");
+    setPersonsState({ 
+      persons: [
+      { name: "Joseph", age:28},
+      { name: "Matt", age:39}
+      ],
+      otherState: personsState.otherState
+    });
+  };
+  
+  return (
+    <div className="App">
+      <hi>Hi, I'm a react app</hi>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person 
+        name={personsState.persons[0].name} 
+        age={personsState.persons[0].age} 
+      />
+      <Person 
+        name={personsState.persons[1].name} 
+        age={personsState.persons[1].age}> My hobbies: Running </Person>
+    </div>    
+  );
+
+};
+//state = {
+//  persons: [
+//    { name: "Joe", age:"28"},
+//    { name: "Matt", age:"31"}
+//  ],
+//  otherState: "Some other value"
+//};  
 
 // Makes the App class default when importing it somewhere else
 export default App;
