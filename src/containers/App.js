c2d694f427254c1452c004e1f4c1b7e32c050e0d
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2020-09-05 15:06:11 -0500

    Organized the project into more specific components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -31,7 +32,7 @@ class App extends Component {
     this.setState({persons: persons});
   }
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = personIndex => {
     //const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
@@ -45,97 +46,27 @@ class App extends Component {
 
   render () {
     let persons = null;
-    let btnClass = [classes.Button];
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)} 
-              name={person.name}  
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangeHandler(person.id, event)}
-              />
-          })}
-        </div>
-      );
-
-      btnClass.push(classes.Red);
-
+      persons = <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}/>
     };
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2)
-      assignedClasses.push(classes.red)
-
-    if (this.state.persons.length <= 1)
-      assignedClasses.push(classes.bold)
 
     return (
       <div className={classes.App}>
-        <hi>Hi, I'm a react app</hi>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button 
-          className={btnClass.join(' ')} 
-          onClick={this.togglePersonsHandler}>Toggle Name
-        </button>
-        
+        <Cockpit 
+        showPersons={this.state.showPersons}
+        persons={this.state.persons} 
+        clicked={this.togglePersonsHandler}/>
         {persons}
-
       </div>
     );
   };
 
 };
 
-  
 
 // Makes the App class default when importing it somewhere else
 export default App;
-
-/*
-const App = props => {
-  const [this.state, setthis.state] = useState({
-    persons: [
-      { name: "Joe", age:28},
-      { name: "Matt", age:31}
-    ]
-  }); 
-
-  const [otherState, setOtherState] = useState({
-    otherState:"Some other value"
-  })
-
-  console.log(this.state, otherState)
-  
-  const switchNameHandler = (newName) => {
-    // console.log("Was Clicked!!!!!!!");
-    setthis.state({ 
-      persons: [
-      { name: newName, age:28},
-      { name: "Matt", age:39}
-      ],
-      otherState: this.state.otherState
-    });
-  };
-  
-  return (
-    <div className="App">
-      <hi>Hi, I'm a react app</hi>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} 
-      />
-      <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={switchNameHandler} >My hobbies: Running</Person>
-    </div>    
-  );
-
-};
-*/
